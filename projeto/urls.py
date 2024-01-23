70a28d93b745691caed10624d31a1665581e1ad0
Baruc1996 <jonatan.borges2012@hotmail.com>
2024-01-23 01:32:19 -0300

    Atualização do projeto

--- projeto/urls.py
+++ projeto/urls.py
@@ -7,3 +7,7 @@ urlpatterns = [
     path('', include('livraria.urls'))
 ]
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
